f19f942077ec4cd9ad6cbb53090a0ca399fea001
Lienchi <Lienchi.Lai@gmail.com>
2018-04-25 22:07:02 +0800

    add user_id to post model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_25_135327) do
+ActiveRecord::Schema.define(version: 2018_04_25_140318) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -32,6 +32,8 @@ ActiveRecord::Schema.define(version: 2018_04_25_135327) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
