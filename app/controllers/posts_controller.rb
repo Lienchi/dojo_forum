a239dfc5b27a4682d5a0a08c95a3aeeeee369668
Lienchi <Lienchi.Lai@gmail.com>
2018-04-26 21:23:24 +0800

    add post new and create actions and views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,4 +4,26 @@ class PostsController < ApplicationController
   def index
     @posts = Post.page(params[:page]).per(10)
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user = current_user
+    if @post.save
+      flash[:notice] = "post was successfully created"
+      redirect_to root_path
+    else
+      flash[:alert] = "post was failed to create"
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
 end
