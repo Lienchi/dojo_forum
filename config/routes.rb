8180ef57da49e0b0eab58eff5e58c64e9e2f3334
Lienchi <Lienchi.Lai@gmail.com>
2018-04-27 00:48:25 +0800

    add comment index action and view

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root "posts#index"
-  resources :posts
+  resources :posts do
+    resources :comments
+  end
 
   namespace :admin do
     root "posts#index"
