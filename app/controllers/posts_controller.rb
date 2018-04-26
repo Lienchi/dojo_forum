c07f83f54719206c55a87cb11604a83f7b1f6904
Lienchi <Lienchi.Lai@gmail.com>
2018-04-26 21:57:36 +0800

    add post update and destroy actions and views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def index
     @posts = Post.page(params[:page]).per(10)
@@ -25,7 +26,26 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def update
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+      flash[:notice] = "post was successfully updated"
+    else
+      render :edit
+      flash[:notice] = "post was failed to update"
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to root_path
+  end
+
   private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
 
   def post_params
     params.require(:post).permit(:title, :content)
